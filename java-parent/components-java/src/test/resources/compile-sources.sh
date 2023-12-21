c9bd0a22beb1873db79301677e6222ac35a1b7f8
Christian Franzen <13965040+cfranzen@users.noreply.github.com>
2023-12-21 23:11:53 +0100

    Compile all Java test sources via shell script

--- java-parent/components-java/src/test/resources/compile-sources.sh
+++ java-parent/components-java/src/test/resources/compile-sources.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+INPUT_DIR=javasources
+OUTPUT_DIR=javaclasses
 TARGET_VERSION=21
 
-javac -d javaclasses -target ${TARGET_VERSION} javasources/de/dummy/project/enums/MySimpleEnum.java
+SOURCES=$(find ${INPUT_DIR} -name "*.java")
+if [ ! -z "$SOURCES" ]; then
+    javac -d ${OUTPUT_DIR} -target ${TARGET_VERSION} $SOURCES
+    if [ $? != 0 ]; then exit; fi
+fi
